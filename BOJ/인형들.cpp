9a45705cb62fe3488c5295d6e6afc6caab3dfc7e
root <j.j.kant1948@gmail.com>
2023-04-30 23:26:49 +0900

    solving BOJ 15954

---
+++ BOJ/인형들.cpp
@@ -0,0 +1,55 @@
+#include <bits/stdc++.h>
+
+#define REP(i, a, b) for (int i = a; i <= b; i++)
+#define PB push_back
+
+using namespace std;
+
+typedef vector<int> vi;
+
+vi v;
+vi dp;
+
+void    get_dispersion(int i, int j){
+
+}
+
+int main(){
+    int n,k;
+
+    cin >> n >> k;
+    int tmp;
+    REP(i, 1, n){
+        cin >>tmp;
+        v.PB(tmp);
+    }
+    long double min = -1;
+    long double t;
+    long double avr;
+    int sum;
+    REP(i, 0, n-k){
+        sum = 0;
+        REP (j, i, n-1){
+            sum += v[j];
+            if (j == i+k-1){
+                t = 0;
+                avr = (long double)sum / (j - i + 1);
+                REP (l, i, j){
+                    t += pow(((long double)v[l] - avr), 2);
+                }
+                min =  t / (j - i + 1);
+            }else if (j > i+k-1){
+                t = 0;
+                avr = (long double)sum / (j - i + 1);
+                REP (l, i, j){
+                    t += pow(((long double)v[l] - avr), 2);
+                }
+                if (min > t / (j - i + 1)){
+                    min = t / (j - i + 1);
+                }
+            }
+        }
+    }
+    cout << sqrt(min) << endl;
+    return (0);
+}
