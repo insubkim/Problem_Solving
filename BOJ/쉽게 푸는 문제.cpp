c7f9e5489026d5ca319ce0e990bb78c08977a755
root <j.j.kant1948@gmail.com>
2023-05-02 23:45:05 +0900

    c

---
+++ BOJ/쉽게 푸는 문제.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+
+#define REP(i, a, b) for (int i = a; i <= b; i++)
+
+using namespace std;
+
+typedef vector<int> vi;
+
+int main(){
+    int n,k;
+
+    cin >> n >> k;
+
+
+}
