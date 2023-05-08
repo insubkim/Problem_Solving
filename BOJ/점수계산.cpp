a6ce30f7c7bdabb7309b68003fe2c635bc7b8853
root <j.j.kant1948@gmail.com>
2023-05-08 23:50:41 +0900

    solved BOJ 점수 계산

--- BOJ/점수계산.cpp
+++ BOJ/점수계산.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <algorithm>
-#include <vector>
+#include <set>
+#include <utility>
+
 
 
 #define REP(i, a, b) for (int i = a; i <= b; i++)
@@ -8,12 +10,39 @@
 
 using namespace std;
 
-typedef vector<int> vi;
+typedef pair<int, int> pii;
 
-vi v;
+
+typedef set<pii> spii;
+
+spii s;
+
+typedef set<int> si;
+si s1;
+
 
 int	main(){
-	
-
-
+    int tmp;
+    REP(i, 1, 8){
+        cin >> tmp;
+        pii p;
+        p.first = tmp;
+        p.second = i;
+        s.insert(p);
+    }
+    int i = 1;
+    int sum = 0;
+    for (auto x : s){
+        if (i >=4){
+            sum += x.first;
+            s1.insert(x.second);
+        }
+        i++;
+    }
+    cout << sum << endl;
+    for (auto x : s1){
+        cout << x <<" ";
+    }
+    cout << endl;
+    return (0);
 }
