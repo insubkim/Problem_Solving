0f178bacc9b9d18097763f55cc2f49a8922851ed
root <j.j.kant1948@gmail.com>
2023-05-10 23:24:30 +0900

    solved : BOJ 일곱 난쟁이

--- BOJ/일곱 난쟁이.cpp
+++ BOJ/일곱 난쟁이.cpp
@@ -4,19 +4,53 @@
 
 using namespace std;
 
-typedef vector<int> vi;
+typedef multiset<int> si;
 
-vi v;
+si v;
+int arr[10] = {0, };
 
+int    solve(int   k){
+    if (k == 8){
+        int sum = 0;
+        std::multiset<int>::iterator it;
+        it = v.begin();
+        REP(i, 1, 9){
+            if (arr[i]){
+                sum += *it;
+            }
+            it++;
+        }
+        it = v.begin();
+        if (sum == 100){
+            REP(i, 1, 9){
+                if (arr[i]){
+                    cout << *it <<endl;
+                }
+                it++;
+            }
+            return (1);
+        }
+    }else{
+        REP(i, k, 9){
+            if (!arr[i]){
+                arr[i] = 1;
+                if (solve(k + 1))
+                    return (1);
+                arr[i] = 0;
+            }
+        }
+    }
+    return (0);
+}
 
 int main(){
 
     int tmp;
     REP(i , 1, 9){
         cin >> tmp;
-        v.push_back(tmp);
+        v.insert(tmp);
     }
-    
+    solve(1);
     return (0);
 }
 
