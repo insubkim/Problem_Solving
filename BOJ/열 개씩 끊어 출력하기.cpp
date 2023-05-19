94f803f60054c0731ba2890b55a6b0a399ece6d5
Insub Kim <j.j.kant1948@gmail.com>
2023-05-19 23:08:47 +0900

    solved : BOJ 알파벳 갯수

--- BOJ/열 개씩 끊어 출력하기.cpp
+++ BOJ/열 개씩 끊어 출력하기.cpp
@@ -7,14 +7,6 @@
 //using namespace std;
 
 int main(){
-	char	c[11] = {0,};
-	int b = 1;
-	while (b){
-		b = scanf("%10s", c);
-		if (b == 10)
-			printf("%s\n", c);
-		else
-			printf("%s", c);
-	}
+	
 	return (0);
 }
