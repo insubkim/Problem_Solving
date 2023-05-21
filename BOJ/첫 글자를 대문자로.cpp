bf10971114c23e05bcb65c098f9f11ab4d52ad4d
MoongMoongBook <j.j.kant1948@gmail.com>
2023-05-21 15:28:24 +0900

    solved : BOJ 알파벳 거리

--- BOJ/첫 글자를 대문자로.cpp
+++ BOJ/첫 글자를 대문자로.cpp
@@ -1,20 +1,12 @@
 //#include <bits/stdc++.h>
 #include <unistd.h>
-
+#include <stdio.h>
 #define REP(i, a, b) for (int i = a; i <= b; i++)
 
 //using namespace std;
 
 int main(){
-    char c;
     int n;
-    scanf("%d", &n);
-    while (n--){
-        scanf("%c", &(c[0]));
-        if ('a' <= c[0] && c[0] <= 'z')
-            c[0] -= 32;
-        scanf("%s\n", &(c[1]));
-        printf("%s\n", c);
-    }
+
     return (0);
 }
