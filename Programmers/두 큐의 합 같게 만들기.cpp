68ef7c00065fdb28c8a759fc3111395d6c1d18f9
Insub Kim <j.j.kant1948@gmail.com>
2023-05-31 03:00:29 +0900

    removed unneccessary

--- Programmers/두 큐의 합 같게 만들기.cpp
+++ Programmers/두 큐의 합 같게 만들기.cpp
@@ -1,6 +1,8 @@
 #include <string>
 #include <vector>
 
+#define PB push_back
+
 using namespace std;
 
 unsigned long long q_sum(vector<int> q){
@@ -11,39 +13,52 @@ unsigned long long q_sum(vector<int> q){
     return (sum);
 }
 
+void	enqueue(vector<int> &q, int val){
+	q.PB(val);
+}
+
+int	dequeue(vector<int> &q){
+	if (q.size() > 0){
+		int val = q[0];
+		q.erase(q.begin());
+		return (val);
+	}else{
+		return (-1);
+	}
+}
+
+#include <iostream>
 int solution(vector<int> q1, vector<int> q2) {
     unsigned long long sum = q_sum(q1) + q_sum(q2);
-    if (sum % 2)
+    if (sum == 0)
+		return (0);
+	if (sum % 2)
         return -1;
     sum = sum / 2;
-    vector<int> q = q1;
-    q.insert(q.end(), q2.begin(), q2.end());
-    int s = 0;
-    int e = (int)q1.size() -1;
-    
-    int move = 0;
-    
-    unsigned long long q1_sum;
-    /*
-        문제점.
-        q1 크기가 0이면 e가 -1이됨
-        e가 q.size()보다 커지면 0 부터 다시 시작해여함.
-        그리고 덧셈도 다시만들어야함.
-    */
-    while (s < (int)q.size()){
-        q1_sum = 0;
-        for (int i = s; i <= e; i++){
-            q1_sum += q[i];
-        }
-        if (q1_sum > sum){
-            s++;
-        }else if (q1_sum < sum){
-            e++;
-        }else{
-            return (move);
-        }
-        move++;
-    }
+	int	move = 0;
+	unsigned long long sum1 = q_sum(q1);
+	int	size = (int)q1.size() + (int)q2.size();
+	int i = 0;
+	int j = 0;
+	int val;
+	while (i < size && j < size){
+		if (sum == sum1){
+			return (move);
+		}else if (sum < sum1){
+			if (i == q1.size())
+				return -1;
+			val = q1[i++];
+			q2.PB(val);
+			sum1 -= val;
+		}else{
+			if (j == q2.size())
+				return -1;
+			val = q2[j++];
+			q1.PB(val);
+			sum1 += val;
+		}
+		move++;
+	}
     return -1;
 }
 
