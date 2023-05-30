95f6437cf03b5332c8d88ead3d51a7788d34e278
Insub Kim <j.j.kant1948@gmail.com>
2023-05-31 05:09:58 +0900

    solved : BOJ 가장 큰 증가하는 부분 수열

--- BOJ/가장 큰 증가하는 부분 수열.cpp
+++ BOJ/가장 큰 증가하는 부분 수열.cpp
@@ -1,9 +1,42 @@
-#include <bits/stdc++.h>
+//#include <bits/stdc++.h>
+#include <iostream>
+#include <vector>
+
 
 #define REP(i, a, b) for (int i = a; i <= b; i++)
+#define PB push_back
 
 using namespace std;
 
+vector<int> v1;
+vector<int> dp;
 int main(){
-
+	int n;
+	cin >> n;
+	int tmp;
+	REP(i, 1, n){
+		cin >> tmp;
+		v1.PB(tmp);
+		dp.PB(tmp);
+	}
+	for (int i = 0; i < n; i++){
+		for (int j = 0; j < i; j++){
+			if (v1[j] < v1[i] && dp[j] + v1[i] > dp[i]){
+				dp[i] = dp[j] + v1[i];
+			} 
+		}
+	}
+	int mx = 0;
+	for (int i = 0; i < n; i++){
+		if (dp[i] > dp[mx])
+			mx = i;
+	}
+	// int len = 1;
+	// for (int i = mx - 1; i >= 0; i--){
+	// 	if (v1[i] < v1[mx]){
+	// 		len++;
+	// 		mx = i;
+	// 	}
+	// }
+	cout << dp[mx] << endl;
 }
