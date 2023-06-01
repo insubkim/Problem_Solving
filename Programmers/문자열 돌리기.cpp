69f2950476f17600a89319e5dc9b56b486b405d2
MoongMoongBook <j.j.kant1948@gmail.com>
2023-06-02 02:35:03 +0900

    solved :PG 문자열 돌리기

---
+++ Programmers/문자열 돌리기.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(void) {
+    string str;
+    cin >> str;
+    for (auto x : str){
+        cout << x << endl;
+    }
+    return 0;
+}
