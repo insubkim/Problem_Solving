954f636a0cdb1b29433ddf9e9ec6822639cf5a91
MoongMoongBook <j.j.kant1948@gmail.com>
2023-06-02 02:38:47 +0900

    solved : PG 홀짝 구분하기

---
+++ Programmers/홀짝구분하기.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void) {
+    int n;
+    cin >> n;
+    if (n % 2){
+        cout << "even\n";
+    }else{
+        cout << "odd\n";
+    }
+    return 0;
+}
