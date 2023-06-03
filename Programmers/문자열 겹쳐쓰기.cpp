9fdebd571805711f894bafae9d72f55dd0cfbd52
root <j.j.kant1948@gmail.com>
2023-06-04 00:00:11 +0900

    solved : Programmers  문자열 겹쳐쓰기

--- Programmers/문자열 겹쳐쓰기.cpp
+++ Programmers/문자열 겹쳐쓰기.cpp
@@ -4,10 +4,11 @@
 using namespace std;
 
 string solution(string m, string o, int s) {
+    string mm = m;
     for (int i = 0; i < (int)o.size(); i++){
-        m[s++] = o[i];
+        mm[s++] = o[i];
     }
-    return (m);
+    return (mm);
 }
 
 int main(){
