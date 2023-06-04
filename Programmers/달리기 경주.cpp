a85a9fb2a95c1ddf9847e7762e801d3bcb10ac64
root <j.j.kant1948@gmail.com>
2023-06-04 21:14:41 +0900

    solved : programmers 달리기 경주

--- Programmers/달리기 경주.cpp
+++ Programmers/달리기 경주.cpp
@@ -1,9 +1,24 @@
 #include <string>
 #include <vector>
+#include <map>
 
 using namespace std;
 
 vector<string> solution(vector<string> p, vector<string> c) {
-    vector<string> answer;
-    return answer;
+    map<string, int> m;
+    for (int i = 0; i < p.size(); i++){
+        map[p[i]] = 0;
+    }
+    int r;
+    string s;
+    for (auto x : c){
+        r = map[x];
+        if (r == 0){
+            continue ;
+        }
+        //map update
+        //p update
+    }
+
+    return p;
 }
