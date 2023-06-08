a120ccd45f487f2be92431f21297755cdf42acfc
root <j.j.kant1948@gmail.com>
2023-06-08 13:38:38 +0900

    solved : programmers 사과장수

---
+++ Programmers/Lv1/사과장수.cpp
@@ -0,0 +1,18 @@
+#include <string>
+#include <vector>
+#include <algorighm>
+
+using namespace std;
+
+int solution(int k, int m, vector<int> s) {
+    int a = 0;
+    sort(s.begin(), s.end());
+    int c = 0;
+    for (int i = s.size() - 1; i >= 0; i--){
+        c++;
+        if (c % m == 0){
+            a += m * s[i];
+        }
+    }
+    return a;
+}
