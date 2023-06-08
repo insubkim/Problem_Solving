043bcb4821e73463332470f9ef2798079f1d2d2a
MoongMoongBook <j.j.kant1948@gmail.com>
2023-06-09 00:07:19 +0900

    solved : programmers  마지막 원소 수

---
+++ Programmers/Lv0/마지막 원소 수.cpp
@@ -0,0 +1,13 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(vector<int> n) {
+    int l = (int)n.size() - 1;
+    if (n[l] > n[l - 1])
+        n.push_back(n[l] - 1);
+    else
+        n.push_back(n[l] * 2);
+    return n;
+}
