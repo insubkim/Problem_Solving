94c35b000923eff9999429e009458c019f5c2eb3
root <j.j.kant1948@gmail.com>
2023-06-11 19:28:05 +0900

    solved : programmers 나머지가 1이 되는 수 찿기

---
+++ Programmers/Lv1/나머지가 1이 되는 수 찾기.cpp
@@ -0,0 +1,13 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int n) {
+    int a;
+    for (a = 2; a *a < n - 1; a++){
+        if (n % a == 1)
+            return a;
+    }
+    return n - 1;
+}
