db8c1b72115745d78587dafeb84f3ec0d2f02a60
Insub Kim <j.j.kant1948@gmail.com>
2023-06-22 17:46:55 +0900

    solved : programmers 3진법 뒤집기

---
+++ Programmers/Lv3/tmp3.cpp
@@ -0,0 +1,59 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+bool	cmp1(vector<int> i, vector<int> j){
+	if (i[0] < j[0])
+		return true;
+	else if (i[0] > j[0])
+		return false;
+	else if (i[1] > j[1])
+		return true;
+	return false;
+}
+
+bool	sum(vector<int> i, vector<int> j){
+	if (i[0] + i[1] > j[0] + j[1])
+		return true;
+	return false;
+}
+
+int solution(vector<vector<int>> s) {
+	int first1 = s[0][0];
+	int first2 = s[0][1];
+	sort(s.begin(), s.end(), cmp1);
+	int	m = s[0][1];
+	for (auto &x : s){
+		if (x[0][1] < m){
+			if (x[0] == first1 && x[1] == first2)
+				return -1;
+			x[0][0] = -1;
+		}
+	}
+
+
+	sort(s.begin(), s.end(), sum);
+	int a = 1;
+	for (auto x : s){
+		if (x[0] == first1 && x[1] == first2)
+			return a;
+		a++;
+	}
+    return -1;
+}
+
+#include <iostream>
+int main(){
+	vector<int> a1 = {2,2};
+	vector<int> a2 = {1,4};
+	vector<int> a3 = {3,2};
+	vector<int> a4 = {3,2};
+	vector<int> a5 = {2,1};
+
+	vector<vector<int>> k = {a1,a2,a3,a4,a5};
+	int  a = solution(k);
+	cout << a << endl;
+	return 0;
+}
