41fbacc8c05393bdcc5d1e3763af27b12d4c899e
MoongMoongBook <j.j.kant1948@gmail.com>
2023-06-28 22:58:52 +0900

    solved : programmers 문자열을정수로바꾸기

---
+++ Programmers/Lv1/문자열을정수로바꾸기.cpp
@@ -0,0 +1,16 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(string s) {
+    return stoi(s);
+}
+
+
+#include <iostream>
+int main(){
+
+    int a = solution("-1");
+    cout<< a << endl;
+}
---
+++ Programmers/Lv1/휴대폰 번호 가리기.cpp
@@ -0,0 +1,10 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(string p) {
+    for (int i = p.size() - 5; i >= 0; i++)
+        p[i] = '*';
+    return a;
+}
