a2380aed1a32570a1d3937b9d49a3e6b05251d24
Insub Kim <j.j.kant1948@gmail.com>
2023-06-29 16:30:19 +0900

    solved : programmers 리코쳇로봇

--- Programmers/Lv2/리코쳇로봇.cpp
+++ Programmers/Lv2/리코쳇로봇.cpp
@@ -1,92 +1,106 @@
 #include <string>
 #include <vector>
-#include <tuple>
+#include <utility>
 #include <queue>
 
 using namespace std;
 
 int solution(vector<string> b) {
     int a = -1;
-    vector<string> visited = b;
-    tuple<int, int, int> loc;
+	vector<string> visited = b;
+    pair<int, int> loc;
     for (int i = 0; i < (int)b.size(); i++){
         for (int j = 0; j < (int)b[i].size(); j++){
             if (b[i][j] == 'R'){
-                get<0>(loc) = i;
-                get<1>(loc) = j;
-                get<2>(loc) = 0;
+                loc.first = i;
+                loc.second = j;
                 a = 0;
-                break ;
+				break ;
             }
         }
         if (a == 0)
             break ;
     }
-    queue <tuple<int, int, int>> bfs;
-    int wid = b[0].size();
+    queue <pair<int, int>> bfs;
+    queue <int> num;
+	int wid = b[0].size();
     int hei = b.size();
+	int	x,y;
     bfs.push(loc);
-    while (bfs.size()){
-        loc = bfs.front();
-        if (visited[get<0>(loc)][get<1>(loc)] == 'G'){
-            return get<2>(loc);
-        }
-        bfs.pop();
-        visited[get<0>(loc)][get<1>(loc)] == 'R';        
-        // 위 아래 오른 왼 방문하고 방문 않했으면 큐에 넣기
-        for (int i = 1; get<0>(loc) - i >= 0; i++){
-            //'d' 이고 이전이 '.' 혹은  끝이 '.' 일때는 방문 가능.
-            if (get<0>(loc) - i == 0 && visited[get<0>(loc) - i][get<1>(loc)] == '.'){
-                bfs.push(make_tuple(get<0>(loc) - i, get<1>(loc), get<2>(loc) + 1));
-                break ;
-            }
-            else if (visited[get<0>(loc) - i][get<1>(loc)] == 'D' && visited[get<0>(loc) - i + 1][get<1>(loc)] == '.'){
-                bfs.push(make_tuple(get<0>(loc) - i + 1, get<1>(loc), get<2>(loc) + 1));
-                break ;
-            }
-        }
-        for (int i = 1; get<0>(loc) + i < hei; i++){
-            if (get<0>(loc) + i == hei - 1 && visited[get<0>(loc) + i][get<1>(loc)] == '.'){
-                bfs.push(make_tuple(get<0>(loc) + i, get<1>(loc), get<2>(loc) + 1));
-                break ;
-            }
-            else if (visited[get<0>(loc) + i][get<1>(loc)] == 'D' && visited[get<0>(loc) + i - 1][get<1>(loc)] == '.'){
-                bfs.push(make_tuple(get<0>(loc) + i - 1, get<1>(loc), get<2>(loc) + 1));
-                break ;
-            }
-        }
-        for (int i = 1; get<1>(loc) - i >= 0; i++){
-            if (get<1>(loc) - i == 0 && visited[get<0>(loc)][get<1>(loc) - i] == '.'){
-                bfs.push(make_tuple(get<0>(loc), get<1>(loc) - i, get<2>(loc) + 1));
-                break ;
-            }
-            else if (visited[get<0>(loc)][get<1>(loc) - i] == 'D' && visited[get<0>(loc)][get<1>(loc) - i + 1] == '.'){
-                bfs.push(make_tuple(get<0>(loc), get<1>(loc) - i + 1, get<2>(loc) + 1));
-                break ;
-            }
-        }
-        for (int i = 1; get<1>(loc) + i < wid; i++){
-            if (get<1>(loc) - i == wid - 1 && visited[get<0>(loc)][get<1>(loc) + i] == '.'){
-                bfs.push(make_tuple(get<0>(lonc), get<1>(loc) + i, get<2>(loc) + 1));
-                break ;
-            }
-            else if (visited[get<0>(loc)][get<1>(loc) + i] == 'D' && visited[get<0>(loc)][get<1>(loc) + i - 1] == '.'){
-                bfs.push(make_tuple(get<0>(loc), get<1>(loc) + i - 1, get<2>(loc) + 1));
-                break ;
-            }
-        }
-    }
+	num.push(0);
+	while (bfs.size()){
+		loc = bfs.front();
+		a = num.front();
+		bfs.pop();
+		num.pop();
+		y = loc.first;
+		x = loc.second;
+		if (visited[y][x] == 'G')
+			return a;
+		visited[y][x] = 'R';
+		a++;
+		//down
+		for (int i = y + 1; i < hei; i++){
+			if (((visited[i][x] == '.' || visited[i][x] == 'G') && i == hei - 1)){
+				bfs.push(make_pair(i, x));
+				num.push(a);
+				break ;
+			}else if(visited[i][x] == 'D'){
+				if ((visited[i - 1][x] == '.' || visited[i - 1][x] == 'G')){
+					bfs.push(make_pair(i - 1, x));
+					num.push(a);
+				}
+				break ;
+			}
+		}
+		//up
+		for (int i = y - 1; i >= 0; i--){
+			if (((visited[i][x] == '.' || visited[i][x] == 'G') && i == 0)){
+				bfs.push(make_pair(i, x));
+				num.push(a);
+				break ;
+			}else if(visited[i][x] == 'D'){
+				if ((visited[i + 1][x] == '.' || visited[i + 1][x] == 'G')){
+					num.push(a);
+					bfs.push(make_pair(i + 1, x));
+				}
+				break ;
+			}
+		}
+		//left
+		for (int i = x - 1; i >= 0; i--){
+			if (((visited[y][i] == '.' || visited[y][i] == 'G') && i == 0)){
+				num.push(a);	
+				bfs.push(make_pair(y, i));
+				break ;
+			}else if(visited[y][i] == 'D'){
+				if ((visited[y][i + 1] == '.' || visited[y][i + 1] == 'G')){
+					num.push(a);
+					bfs.push(make_pair(y, i + 1));
+				}break ;
+			}
+		}
+		//right
+		for (int i = x + 1; i <= wid; i++){
+			if (((visited[y][i] == '.' || visited[y][i] == 'G') && i == wid - 1)){
+				bfs.push(make_pair(y, i));
+				num.push(a);
+				break ;
+			}else if(visited[y][i] == 'D'){
+				if ((visited[y][i - 1] == '.' || visited[y][i - 1] == 'G')){
+					bfs.push(make_pair(y, i - 1));
+					num.push(a);
+				}
+				break ;
+			}
+		}
+	}
     return -1;
 }
 
-
 #include <iostream>
 int main(){
-    string a = "..R";
-    string b = "...";
-    string c = "G..";
-    vector<string> k = {a,b,c,};
-    int j = solution(k);
-    cout << j << endl;
+	vector<string> a = {"..." , "...", "R.G"};
+	int k = solution(a); 
+	cout << k << endl;
 }
-
