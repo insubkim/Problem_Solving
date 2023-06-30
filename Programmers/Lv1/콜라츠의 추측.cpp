f4842b52e8de44b2ba1667934ec28fc4bc34929e
MoongMoongBook <j.j.kant1948@gmail.com>
2023-06-30 22:58:38 +0900

    solving : 콜라츠의 추측 , other way'

--- Programmers/Lv1/콜라츠의 추측.cpp
+++ Programmers/Lv1/콜라츠의 추측.cpp
@@ -4,22 +4,23 @@
 using namespace std;
 
 int solution(int n) {
-    long long num = n;
     if (n < 0)
             return -1;
-    if (num == 1)
+    if (n == 1)
         return 0;
     int i = 0;
     while (i < 500){
+        n = n % 2 ? n * 3 + 1 : n / 2;
         i++;
-        if (num % 2 == 0){
-            num /= 2;
-            if (num == 1)
-                return i;
-        }else{
-            num *= 3;
-            num++;
+        if (n == 1){
+            return i;
         }
     }
     return -1;
+}
+
+#include <iostream>
+int main(){
+    int k = solution(626331);
+    cout << k << endl;
 }
