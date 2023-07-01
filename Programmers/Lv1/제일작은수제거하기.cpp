fae0ffee990e1d7a5bdce781b7dcad0fea9436bb
MoongMoongBook <j.j.kant1948@gmail.com>
2023-07-01 11:44:31 +0900

    solved :programmers 제일 작은수

---
+++ Programmers/Lv1/제일작은수제거하기.cpp
@@ -0,0 +1,26 @@
+#include <string>
+#include <vector>
+#inlclude <algorithm>
+
+using namespace std;
+
+// vector<int> solution(vector<int> arr) {
+//     int m = 0;
+//     for (int i = 1; i (int)arr.size(); i++){
+//         if (arr[m] > arr[i]){
+//             m = i;
+//         }
+//     }
+//     arr.erase(arr.begin() + m);
+//     if (arr.size() == 0){
+//         arr.push_back(-1);
+//     }
+//     return arr;
+// }
+
+vector<int> solution(vector<int> a){
+    int m = *min_element(a.begin(), a.end());
+    m = find(a.begin(), a.end(), m) - a.begin();
+    a.erase(a.begin() + m);
+    return a.size() > 0 ? a : a.push_back(-1);
+}
