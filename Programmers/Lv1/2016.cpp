3b3ff2a08612e304d02c16a5283f4c73fee8c2eb
MoongMoongBook <j.j.kant1948@gmail.com>
2023-07-05 23:49:34 +0900

    solved : programmers 2016

--- Programmers/Lv1/2016.cpp
+++ Programmers/Lv1/2016.cpp
@@ -4,6 +4,36 @@
 using namespace std;
 
 string solution(int a, int b) {
-    string answer = "";
-    return answer;
+    int i = 4;
+    for (int j = 1; j < a; j++){
+        if (j == 1 || j == 3 || j == 5 || j == 7 || j == 8 || j == 10)
+            i += 31;
+        else if (j == 2)
+            i += 29;
+        else
+            i += 30;
+    }
+    i = (i + b) % 7;
+    switch (i)
+    {
+    case 0:
+        return "SUN";
+    case 1:
+        return "MON";
+    case 2:
+        return "TUE";
+    case 3:
+        return "WED";
+    case 4:
+        return "THU";
+    case 5:
+        return "FRI";
+    default:
+        return "SAT";
+    }
+}
+
+#include <iostream>
+int main(){
+    cout << solution(9,29) << endl;
 }
--- Programmers/Lv1/같은숫자는싫어.cpp
+++ Programmers/Lv1/같은숫자는싫어.cpp
@@ -6,6 +6,10 @@ using namespace std;
 vector<int> solution(vector<int> arr) 
 {
     vector<int> a;
+    if (arr.size() == 0){
+        return a;
+    }
+    a.push_back(arr[0]);
     for (auto x : arr){
         if (a.back() != x)
             a.push_back(x);
