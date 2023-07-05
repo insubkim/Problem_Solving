8fbc7020ffd9e27a3f888e9879a8960d99b1a82d
MoongMoongBook <j.j.kant1948@gmail.com>
2023-07-06 00:14:26 +0900

    solved : programmers 최대공약수와최소공배수

---
+++ Programmers/Lv1/최대공배수와최소공배수.cpp
@@ -0,0 +1,23 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(int n, int m) {
+    int tmp = n;
+    if (n > m){
+        n = m; 
+        m = tmp;
+    }
+    vector<int> a;
+    tmp = -1;
+    for (int i = 1; i <= n; i++){
+        if (m % i == 0 && n & i == 0)
+            tmp = i;
+    }
+    a.push_back(tmp);
+    tmp = i
+    for (tmp = 1; (tmp * m) % n; tmp++) {}
+    a.push_back(tmp);
+    return a;
+}
