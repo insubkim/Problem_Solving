37ecac12e77c268c42e44ec82c19e11a697e9aa2
MoongMoongBook <j.j.kant1948@gmail.com>
2023-07-26 22:38:18 +0900

    solved : programmers Lv2 미로탈출

--- Programmers/Lv2/미로 탈출.cpp
+++ Programmers/Lv2/미로 탈출.cpp
@@ -2,6 +2,7 @@
 #include <vector>
 #include <queue>
 #include <utility>
+#include <tuple>
 
 
 using namespace std;
@@ -9,45 +10,16 @@ using namespace std;
 int px = 0;
 int py = 0;
 
-int lx = 0;
-int ly = 0;
-
-int ex = 0;
-int ey = 0;
-
 int w;
 int h;
 
-void    get(vector<string> maps){
-    for(int y = 0; y < h; y++){
-        for(int x = 0; x < w; x++){
-            if (maps[y][x] == 'S')
-            {
-                px = x;
-                py = y;
-            }
-            else if (maps[y][x] == 'L')
-            {
-                lx = x;
-                ly = y;
-            }
-            else if (maps[y][x] == 'E')
-            {
-                ex = x;
-                ey = y;
-            }
-        }   
-    }
-}
-
-
-int bfs(vector<string> maps, int px, int py, int ex, int ey){
+int bfs(vector<string> maps, int px, int py, char c){
     queue<pair<int, int>> q;
     queue<int> qt;
     int    t;
     q.push(make_pair(px, py));
     qt.push(0);
-    pair<int,int>    p;
+    maps[py][px] = 'X';
     while (q.size() > 0){
         //dequeue
         px = q.front().first;
@@ -55,29 +27,41 @@ int bfs(vector<string> maps, int px, int py, int ex, int ey){
         q.pop();
         t = qt.front();
         qt.pop();
-        //목표지점인지 확인
-        if (px == ex && py == ey)
-            return t;
         t++;
-        maps[py][px] = 'X';
         //주변에 visit 안햇으면 enqueue
         //위
         if (py - 1 >= 0 && maps[py - 1][px] != 'X'){
+            //목표지점인지 확인
+            if (maps[py - 1][px] == c)
+                return t;
+            maps[py - 1][px] = 'X';
             q.push(make_pair(px, py - 1));
             qt.push(t);
         }
         //왼쪽
         if (px - 1 >= 0 && maps[py][px - 1] != 'X'){
+            //목표지점인지 확인
+            if (maps[py][px - 1] == c)
+                return t;
+            maps[py][px - 1] = 'X';
             q.push(make_pair(px - 1, py));
             qt.push(t);
         }
         //아래
         if (py + 1 < h && maps[py + 1][px] != 'X'){
+            //목표지점인지 확인
+            if (maps[py + 1][px] == c)
+                return t;
+            maps[py + 1][px] = 'X';
             q.push(make_pair(px, py + 1));
             qt.push(t);
         }
         //오른쪽
         if (px + 1 < w && maps[py][px + 1] != 'X'){
+            //목표지점인지 확인
+            if (maps[py][px + 1] == c)
+                return t;
+            maps[py][px + 1] = 'X';
             q.push(make_pair(px + 1, py));
             qt.push(t);
         }
@@ -92,12 +76,30 @@ int solution(vector<string> maps) {
     w = (int)maps[0].size();
     h = (int)maps.size();
 
-    get(maps);
-    tmp = bfs(maps, px, py, lx, ly);
+    size_t pos;
+    for (int i = 0; i < h; i++){    
+        pos = maps[i].find('S' ,0);
+        if (pos != string::npos)
+        {   
+            px = (int)pos;
+            py = i;
+            break ;
+        }
+    }
+    tmp = bfs(maps, px, py, 'L');
     if (tmp == -1)
         return -1;
     a += tmp;
-    tmp = bfs(maps, lx, ly, ex, ey);
+    for (int i = 0; i < h; i++){    
+        pos = maps[i].find('L' ,0);
+        if (pos != string::npos)
+        {   
+            px = (int)pos;
+            py = i;
+            break ;
+        }
+    }
+    tmp = bfs(maps, px, py, 'E');
     if (tmp == -1)
         return -1;
     return a + tmp;
