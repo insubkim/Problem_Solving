8e656a62fbc282ba1f516bb0f89742c079d49b6d
MoongMoongBook <j.j.kant1948@gmail.com>
2023-07-29 23:05:38 +0900

    solved : BOJ 첫글자를 대문자로

--- BOJ/첫 글자를 대문자로.cpp
+++ BOJ/첫 글자를 대문자로.cpp
@@ -1,12 +1,22 @@
-//#include <bits/stdc++.h>
+#include <iostream>
 #include <unistd.h>
 #include <stdio.h>
+#include <ctype.h>
 #define REP(i, a, b) for (int i = a; i <= b; i++)
 
-//using namespace std;
+using namespace std;
 
 int main(){
     int n;
-
+    cin >> n;
+    string s;
+    cin.ignore();
+    while (n--)
+    {
+        getline(cin, s);
+        if (s.size() > 0)
+            s[0] = toupper(s[0]);
+        cout << s << endl;
+    }
     return (0);
 }
