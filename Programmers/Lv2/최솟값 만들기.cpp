ffa7592c87a53fc1293ffc6c72455fd406df4391
inskim <j.j.kant1948@gmail.com>
2023-12-15 09:52:48 +0900

    solved: programmers 최솟값 만들기 inner product

--- Programmers/Lv2/최솟값 만들기.cpp
+++ Programmers/Lv2/최솟값 만들기.cpp
@@ -1,21 +1,15 @@
-#include <iostream>
 #include <vector>
 #include <algorithm>
+#include <numeric>
 
 using namespace std;
 
 int solution(vector<int> A, vector<int> B)
 {
   int answer = 0;
-  
+
   sort(A.begin(), A.end());
-  sort(B.begin(), B.end());
-  vector<int>::iterator         it_A  = A.begin();
-  vector<int>::reverse_iterator rit_B = B.rbegin();
-
-  while (it_A != A.end()) {
-    answer += *it_A++ * *rit_B+;
-  }
-
+  sort(B.rbegin(), B.rend());
+  answer = inner_product(A.begin(), A.end(), B.begin(), 0);
   return answer;
 }
