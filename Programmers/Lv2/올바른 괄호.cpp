79f62b1baaf07c78c5c68612a4521fb8f9974fc7
insub <j.j.kant1948@gmail.com>
2023-12-17 15:20:22 +0900

    solved: programmers 올바른 기호

---
+++ Programmers/Lv2/올바른 괄호.cpp
@@ -0,0 +1,16 @@
+#include<string>
+#include <iostream>
+#include <stack>
+
+using namespace std;
+
+bool solution(string s)
+{
+  stack<char> st;
+  for (auto x : s) {
+    if (x == '(')       st.push(')');
+    else if (st.empty()) return false;
+    else                st.pop(); 
+  }
+  return st.empty() ? true : false;
+}
