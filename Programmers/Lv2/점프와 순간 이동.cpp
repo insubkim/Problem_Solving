6dd6d4c02cceb4729be26e45ee15818be7b05ef1
inskim <j.j.kant1948@gmail.com>
2023-12-26 09:44:39 +0900

    solved: programmers 점프와 순간이동 bit 연산이용한 최적화

--- Programmers/Lv2/점프와 순간 이동.cpp
+++ Programmers/Lv2/점프와 순간 이동.cpp
@@ -1,16 +1,49 @@
+
 #include <iostream>
-
 using namespace std;
-
-int solve(int n, int battery) {
-  if (n == 0)
-    return battery;
-  if (n % 2 && battery != 0) return solve(n / 2, battery);
-  return solve(n - 1, battery + 1);
-}
 
 int solution(int n)
 {
-  int ans = solve(n, 0);  
-  return ans;
+    int ans = 0;
+
+    ans += (n & 0x00000001 ? 1 : 0);
+    ans += (n & 0x00000002 ? 1 : 0);
+    ans += (n & 0x00000004 ? 1 : 0);
+    ans += (n & 0x00000008 ? 1 : 0);
+    ans += (n & 0x00000010 ? 1 : 0);
+    ans += (n & 0x00000020 ? 1 : 0);
+    ans += (n & 0x00000040 ? 1 : 0);
+    ans += (n & 0x00000080 ? 1 : 0);
+    ans += (n & 0x00000100 ? 1 : 0);
+    ans += (n & 0x00000200 ? 1 : 0);
+    ans += (n & 0x00000400 ? 1 : 0);
+    ans += (n & 0x00000800 ? 1 : 0);
+    ans += (n & 0x00001000 ? 1 : 0);
+    ans += (n & 0x00002000 ? 1 : 0);
+    ans += (n & 0x00004000 ? 1 : 0);
+    ans += (n & 0x00008000 ? 1 : 0);
+    ans += (n & 0x00010000 ? 1 : 0);
+    ans += (n & 0x00020000 ? 1 : 0);
+    ans += (n & 0x00040000 ? 1 : 0);
+    ans += (n & 0x00080000 ? 1 : 0);
+    ans += (n & 0x00100000 ? 1 : 0);
+    ans += (n & 0x00200000 ? 1 : 0);
+    ans += (n & 0x00400000 ? 1 : 0);
+    ans += (n & 0x00800000 ? 1 : 0);
+    ans += (n & 0x01000000 ? 1 : 0);
+    ans += (n & 0x02000000 ? 1 : 0);
+    ans += (n & 0x04000000 ? 1 : 0);
+    ans += (n & 0x08000000 ? 1 : 0);
+    ans += (n & 0x10000000 ? 1 : 0);
+    ans += (n & 0x20000000 ? 1 : 0);
+    ans += (n & 0x40000000 ? 1 : 0);
+    ans += (n & 0x80000000 ? 1 : 0);
+
+    return ans;
+}
+
+int main(){
+  int a = solution(2);
+  cout << a << endl;
+
 }
