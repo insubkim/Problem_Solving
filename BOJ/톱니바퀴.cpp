030d500f74a9dad111635d5d83931879e8ca46d9
inskim <j.j.kant1948@gmail.com>
2024-01-11 11:44:12 +0900

    solved: BOJ 톱니바퀴

--- BOJ/톱니바퀴.cpp
+++ BOJ/톱니바퀴.cpp
@@ -16,65 +16,70 @@ void  getGear(int gear[][8]) {
   }
 }
 
+int getMagnet(int gear[][8], int head[], int gear_num, int offset) {
+  int idx = (head[gear_num - 1] + offset + 8) % 8;
+  return gear[gear_num - 1][idx];
+}
+
 void  checkLeft(int moved_gear, int head[], int gear[][8], int move[]) {
-  if (moved_gear == 0) return;
+  if (moved_gear == 1) return;
 
   int target_gear = moved_gear - 1;
 
-  int target_gear_magnet3 = gear[target_gear][(head[target_gear] + 2) % 8];
-  int moved_gear_magnet9 = gear[moved_gear][head[moved_gear] - 2 < 0 ? (head[moved_gear] - 2) + 8 : head[moved_gear] - 2];
+  int target_gear_magnet3 = getMagnet(gear, head, target_gear, 2);
+  int moved_gear_magnet9 = getMagnet(gear, head, moved_gear, -2);
 
   if (target_gear_magnet3 != moved_gear_magnet9) {
-    move[target_gear] = -move[moved_gear];
+    move[target_gear - 1] = -move[moved_gear - 1];
     checkLeft(target_gear, head, gear, move);
   }
 }
 
 void  checkRight(int moved_gear, int head[], int gear[][8], int move[]) {
-  if (moved_gear == 3) return;
+  if (moved_gear == 4) return;
 
   int target_gear = moved_gear + 1;
 
-  int moved_gear_magnet3 = gear[moved_gear][(head[moved_gear] + 2) % 8];
-  int target_gear_magnet9 = gear[target_gear][head[target_gear] - 2 < 0 ? (head[target_gear] - 2) + 8 : head[target_gear] - 2];
+  int moved_gear_magnet3 = getMagnet(gear, head, moved_gear, 2);
+  int target_gear_magnet9 = getMagnet(gear, head, target_gear, -2);
 
   if (moved_gear_magnet3 != target_gear_magnet9) {
-    move[target_gear] = -move[moved_gear];
+    move[target_gear - 1] = -move[moved_gear - 1];
     checkRight(target_gear, head, gear, move);
   }
 }
 
 void  init(int move[], int target_gear, int operation) {
   fill_n(&move[0], 4, 0);
-  move[target_gear] = operation;
+  move[target_gear - 1] = operation;
 }
 
 void  moveGear(int head[], int move[]) {
   for (int i = 0; i < 4; i++) {
-    head[i] += move[i];
+    head[i] = (head[i] - move[i] + 8) % 8;
   }
 }
 
 void  solve(int gear[][8]) {
   int operation_cnt;
-  int targetGear;
+  int targetGear;//선택된 기어
   int operation;//1 시계, -1 반시계
-  int head[4] = {0, 0, 0, 0}; // 12시 인덱스
+  int head[4] = {0}; // 0번 : 12시, 1번 : 1시, 2번 : 3시 ... 
   int move[4]; // 움직임 여부
 
   cin >> operation_cnt;
   while (operation_cnt--) {
     cin >> targetGear >> operation;
-    init(move, targetGear - 1, operation);
-    checkLeft(targetGear - 1, head, gear, move);
-    checkRight(targetGear - 1, head, gear, move);
+    init(move, targetGear, operation);
+    checkLeft(targetGear, head, gear, move);
+    checkRight(targetGear, head, gear, move);
     moveGear(head, move);
   }
   int sum = 0;
-  sum += gear[0][head[0]] ? 1 : 0;
-  sum += gear[1][head[1]] ? 2 : 0;
-  sum += gear[2][head[2]] ? 4 : 0;
-  sum += gear[3][head[3]] ? 8 : 0;
+  sum += getMagnet(gear, head, 1, 0) ? 1 : 0;
+  sum += getMagnet(gear, head, 2, 0) ? 2 : 0;
+  sum += getMagnet(gear, head, 3, 0) ? 4 : 0;
+  sum += getMagnet(gear, head, 4, 0) ? 8 : 0;
   cout << sum << endl;
 }
 
