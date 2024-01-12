05c0c6ed708df7debbe082e09391d016688c3c8d
insub <j.j.kant1948@gmail.com>
2024-01-12 20:17:16 +0900

    solved: BOJ 회전하는 큐

--- BOJ/회전하는 큐.cpp
+++ BOJ/회전하는 큐.cpp
@@ -6,42 +6,54 @@
 
 using namespace std;
 
+template <typename T>
+T search_val(T begin, T end, int val) {
+  while (begin != end) {
+    if (*begin == val) {
+      return begin;
+    }
+    begin++;
+  }
+  return begin;
+}
+
+void  rotate(deque<int>& q, int dir) {
+  while (dir > 0) {
+    int val = q.front();
+    q.pop_front();
+    q.push_back(val);
+    dir--;
+  }
+  while (dir < 0) {
+    int val = q.back();
+    q.pop_back();
+    q.push_front(val);
+    dir++;
+  }
+}
+
 int main() {
   int queue_size;
   int extract_size;
 
   cin >> queue_size >> extract_size;
 
-  deque<int> queue(queue_size);
-  iota(queue.begin(), queue.end(), 1);
+  deque<int> q(queue_size);
+  iota(q.begin(), q.end(), 1);
 
   int operation_cnt = 0;
   for (int i = 0; i < extract_size; i++) {
     int extract;
     cin >> extract;
-    deque<int>::iterator it = queue.begin();
-    int a;
-    while (it != queue.end()) {
-      if (*it == extract) {
-        a = it - queue.begin();
-        break;
-      }
-      it++;
+    int a = search_val(q.begin(), q.end(), extract) -  q.begin();
+    int b = search_val(q.rbegin(), q.rend(), extract) - q.rbegin() + 1;
+    if (a < b) {
+      rotate(q, a);
+    } else {
+      rotate(q, -b);
     }
-    deque<int>::reverse_iterator rit = queue.rbegin();
-    while (rit != queue.rend()) {
-      if (*rit == extract) {
-        if (a < rit - queue.rbegin()) {
-          
-        } else {
-
-        }
-        a = min((int)a, (int)(rit - queue.rbegin()));
-        break;
-      }
-      rit++;
-    }
-    operation_cnt += a;
+    q.pop_front();
+    operation_cnt += a < b ? a : b;
   }
   cout << operation_cnt << endl;
 }
