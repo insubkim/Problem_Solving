fcc21de67f71d64e2d5ff27825aee5a92b73e467
inskim <j.j.kant1948@gmail.com>
2024-01-19 09:00:37 +0900

    comment: 주석 수정

--- BOJ/터렛.cpp
+++ BOJ/터렛.cpp
@@ -12,26 +12,30 @@ void    solve(void){
     int x1,y1,r1;
     int x2,y2,r2;
 
-    cin >> x1 >> y1 >> r1 >> x2 >> y2 >> r2;    
+    cin >> x1 >> y1 >> r1 >> x2 >> y2 >> r2;
+    
     // 위치 같을때
     if (x1 == x2 && y1 == y2){
-        cout << r1 == r2 ? 1 : 0 << endl;  
+        if (r1 == r2) // 거리 같음
+            cout << -1 << endl;  
+        else //거리 다름
+            cout << 0 << endl;  
         return ;
     }
     
-    // 거리가 다를 때, 두 점 거리 구함
-    double d = sqrt(pow(x1 - x2, 2) + pow(y1 - y2, 2));
+    //두 점 거리 구함
+    double d = sqrt((x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2));
     
-    if (d == r1 + r2) // 두 점 거리와 각각의 위치 합이 같을때
+    if (d == r1 + r2) // 두 점 거리와 반지름 합 같을때
         cout << 1 << endl;
-    else if (d > r1 + r2){ // 두 점 거리가 각각의 위치 합보다 클때
+    else if (d > r1 + r2){ // 두 점 거리가 반지름 합보다 클때
         cout << 0 << endl;
-    }else{ // 두 점 거리가 각각의 위치 합이 작을때
-        if (abs(r1 - r2) < d) { // 두 원이 접하는 곳이 있고 한점에서 접하진 않음.
+    }else{ // 두 점 거리가 반지름 합보다 작을때
+        if (abs(r1 - r2) < d) { // 그림상, 한원이  다른 원 포함하지 않을때 
             cout << 2 << endl;
-        } else if (abs(r1 - r2) == d) { // 두 점 거리 뺀값이  반지름 뺀 값
+        } else if (abs(r1 - r2) == d) { //그림상, 한원이  다른 원 포함, 접할때
             cout << 1 << endl;
-        } else {
+        } else {//그림상, 한원이  다른 원 포함, 접하지 않을때
             cout << 0 << endl;
         }
     }
