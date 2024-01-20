6b74cbffa5ee7461f5af83a914840fa24134ae20
insub <j.j.kant1948@gmail.com>
2024-01-21 00:04:14 +0900

    solved: BOJ 하키

--- BOJ/하키.cpp
+++ BOJ/하키.cpp
@@ -15,12 +15,12 @@ int main() {
   for (int i = 0; i < P; i++) {
     int x,y;
     cin >> x >> y;
-    if (X <= x <= X + W &&
-        Y <= y <= Y + H) {
+    if (X <= x && x <= X + W && \
+        Y <= y && y <= Y + H) {
             cnt++;
-    } else if (sqrt(pow(X - x, 2) + (Y + r - y)) <= r) {
+    } else if (sqrt(pow(X - x, 2) + pow(Y + r - y, 2)) <= r) {
       cnt++;
-    } else if (sqrt(pow(X + r - x, 2) + (Y + r - y)) <= r) {
+    } else if (sqrt(pow(X + W - x, 2) + pow(Y + r - y, 2)) <= r) {
       cnt++;
     } 
   }
