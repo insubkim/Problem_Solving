9f9c9f0129926f071519626a53b86379f9c7ce54
insub <j.j.kant1948@gmail.com>
2024-01-23 21:18:58 +0900

    solved: BOJ 행렬

--- BOJ/행렬.cpp
+++ BOJ/행렬.cpp
@@ -6,9 +6,9 @@ using namespace std;
 int row, col;
 int A[50][50];
 int B[50][50];
-int cnt = -1;
+int cnt;
 
-bool  isSame(int A[][50]) {
+bool  isSame(void) {
   for (int i = 0; i < row; i++) {
     for (int j = 0; j < col; j++) {
       if (A[i][j] != B[i][j]) return false;
@@ -17,27 +17,24 @@ bool  isSame(int A[][50]) {
   return true;
 }
 
-void  solve(int C[][50], int r, int c, int move_cnt) {
-  if (isSame(C)) {
-    cnt = cnt == -1 ? move_cnt : min(cnt, move_cnt);
-    return ;
-  }
-  if (r > row - 3 || c > col - 3)
-    return ;
-  int D[50][50];
-  memcpy(D, C, 50 * 50);
-  int next_c = (c + 1) > col - 3 ? 0 : c + 1;
-  int next_r = next_c == 0 ? r + 1 : r;
-  //no move
-  solve(D, next_r, next_c, move_cnt);
-
+void  move(int row, int col) {
   for (int i = 0; i < 3; i++) {
     for (int j = 0; j < 3; j++) {
-      D[i + r][j + c] = !D[i + r][j + c];
+      A[i + row][j + col] &= 1;
+    }  
+  }
+}
+
+void  solve(void) {
+  cnt = 0;
+  for (int i = 0; i < row - 2; i++) {
+    for (int j = 0; j < col - 2; j++) {
+      if (A[i][j] == B[i][j]) continue;
+      move(i, j);
+      cnt++;
     }
   }
-  //move
-  solve(D, next_r, next_c, move_cnt + 1);
+  if (!isSame()) cnt = -1;
 }
 
 int main() {
@@ -56,7 +53,6 @@ int main() {
       B[i][j] = s[j] - '0';
     }
   }
-  
-  solve(A, 0, 0, 0);
+  solve();
   cout << cnt << endl;
 }
