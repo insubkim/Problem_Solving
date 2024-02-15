a8fb917b378ceb147dcc25d8a446e3e6ebda6ce9
insub <j.j.kant1948@gmail.com>
2024-02-15 23:22:31 +0900

    solved: BOJ 언더 프라임 시간 개선 600milisecond -> 4milisecond

--- BOJ/언더프라임.cpp
+++ BOJ/언더프라임.cpp
@@ -7,52 +7,34 @@ using namespace std;
 
 int main() {
   
-  int eratostenes[100002] = {0, };
-
+  int eratostenes[100001];
   memset(eratostenes, 1, sizeof eratostenes);
+  eratostenes[0] = 0;
   eratostenes[1] = 0;
-  for (int i = 2; i * i < 100002; i++) {
+  
+  int prime_factors_list[100001];
+  memset(prime_factors_list, 0, sizeof prime_factors_list);
+
+  for (int i = 2; i < 100001; i++) {
     if (!eratostenes[i]) continue;
-    for (int j = i * i; j < 100002; j += i) 
+    for (int j = i * 2; j < 100001; j += i) {
       eratostenes[j] = 0;
+      int tmp = j;
+      while (tmp % i == 0) {
+        tmp /= i;
+        prime_factors_list[j]++;
+      }
+    }
   }
 
   int A, B;
-
   cin >> A >> B;
-  int cnt = 0;
-  
-  int prime_factors_list[100002];
-  memset(prime_factors_list, -1, sizeof prime_factors_list);
-  prime_factors_list[1] = 0;
 
+  int cnt = 0;
   while (A <= B) {
-    int tmp = A;
-    if (eratostenes[A]) {
-      A++;
-      continue;
-    }
-    // 소인수 갯수 구하기
-    int prime_factors = 0;
-    int div = 2;
-    while (tmp != 1) {
-      if (prime_factors_list[tmp] != -1) {
-        prime_factors += prime_factors_list[tmp];
-        break ;
-      }
-      if (tmp % div)  div++;
-      else {
-        tmp /= div;
-        prime_factors++;
-      }
-    }
-    prime_factors_list[A] = prime_factors;
-    // 갯수가 소수인지 판별하기
-    assert(prime_factors > 0);
-    if (eratostenes[prime_factors]) cnt++;
+    if (eratostenes[prime_factors_list[A]]) cnt++;
     A++;
   }
 
   cout << cnt << endl;
-
 }
