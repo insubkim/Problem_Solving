81188215096186d54f15fe7a0c768e5a5615cb06
insub <j.j.kant1948@gmail.com>
2024-03-05 22:49:15 +0900

    solved: BOJ 국회의원 선거, 우선순위 큐 적용

--- BOJ/국회의원 선거.cpp
+++ BOJ/국회의원 선거.cpp
@@ -2,42 +2,37 @@
 #include <cmath>
 #include <cstring>
 #include <algorithm>
-#include <vector>
+#include <queue>
 
 using namespace std;
 
-inline bool  cmp(const int a, const int b) {
-  return a > b;
-}
 
 int main() {
   int N;
   cin >> N;
 
-  vector<int> arr(N - 1);
   int dasom;
   cin >> dasom;
+  int money_dasom = dasom;
+
+  priority_queue<int> q;
   for (int i = 0; i < N - 1; i++) {
-    cin >> arr[i];
+    int tmp;
+    cin >> tmp;
+    q.push(tmp);
   }
   
-  sort(arr.begin(), arr.end(), cmp);
-
-  int money = 0;
-
   if (N == 1) {
     cout << 0 << endl;
     return 0;
   }
-  while (dasom <= arr[0]) {
-    money++;
-    dasom++;
-    arr[0]--;
-    for (int i = 0; i < N - 2; i++) {
-      if (arr[i] < arr[i + 1]) swap(arr[i], arr[i + 1]);
-      else break ;
-    }
+
+  while (q.top() >= money_dasom) {
+    int tmp = q.top();
+    q.pop();
+    q.push(tmp - 1);
+    money_dasom++;
   }
 
-  cout << money << endl;
+  cout << money_dasom - dasom << endl;
 }
