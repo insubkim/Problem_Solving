adda92bc4380799f303d76a29ea73de56180a411
insub <j.j.kant1948@gmail.com>
2024-04-29 23:52:57 +0900

    solved: BOJ홀수일까 짝수일까

---
+++ BOJ/홀수일까 짝수일까.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+  int N;
+  cin >> N;
+  while (N--) {
+    string in;
+    cin >> in;
+    if ((in.back() - '0') % 2) cout << "odd"   << endl;
+    else        cout << "even"  << endl;
+  }
+}
