22d60bb97313aef801e16d1275c8df3594e0e3f0
insub <j.j.kant1948@gmail.com>
2024-05-12 23:26:53 +0900

    solved: BOJ 평범한 배낭

--- BOJ/평범한 배낭.cpp
+++ BOJ/평범한 배낭.cpp
@@ -1,12 +1,30 @@
 #include <iostream>
 #include <cmath>
-#include <cassert>
+
+using namespace std;
+
+int dp[101][100001];
+int item[101][2]; //무게, 가치
 
 int main() {
-  int N, K;//rock, max_weight;
+  int N, K;
   cin >> N >> K;
 
-  int dp[100000] = {0, };
-  int rock[100];
+  for (int i = 1; i <= N; i++) {
+    cin >> item[i][0] >> item[i][1];
+  }
 
+  for (int i = 1; i <= N; i++) {
+    int weight  = item[i][0];
+    int value   = item[i][1];
+    for (int j = 1; j <= K; j++) {
+      if (j >= weight) {
+        dp[i][j] = max(dp[i - 1][j], dp[i - 1][j - weight] + value);
+      } else {
+        dp[i][j] = dp[i - 1][j];
+      }
+    }
+  }
+
+  cout << dp[N][K] << endl;
 }
