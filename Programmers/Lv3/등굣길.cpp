4f98854d43dc5f32f6d71f108b7988fcd1c122bf
insub <j.j.kant1948@gmail.com>
2025-12-29 19:41:01 +0900

    solved: programmers 등굣길

--- Programmers/Lv3/등굣길.cpp
+++ Programmers/Lv3/등굣길.cpp
@@ -9,38 +9,36 @@ int solution(int m, int n, vector<vector<int>> puddles) {
 
     int dp[101][101] = {0, };
 
-    for (int i = 1; i < 101; i++)
-    {
-        dp[1][i] = 1;
-        dp[i][1] = 1;
-    }
-
     for (auto &x: puddles)
     {
         dp[x[1]][x[0]] = -1;
     }
 
+    dp[1][1] = 1;
+
     for (int i = 2; i < 101; i++)
     {
         for (int j = 2; j < 101; j++)
         {
+            if (i == 1 && j == 1) continue;
+
             if (dp[i][j] == -1) continue;
 
-            dp[i][j] = dp[i - 1][j] == -1 ? 0 : dp[i - 1][j]
-                      + dp[i][j - 1] == -1 ? 0 : dp[i][j - 1];
+            dp[i][j] = (dp[i - 1][j] == -1 ? 0 : dp[i - 1][j])
+                      + (dp[i][j - 1] == -1 ? 0 : dp[i][j - 1]);
             dp[i][j] %=  1000000007;
         }
     }
 
 
-    for (int i = 1; i <= 3; i++)
-    {
-        for (int j = 1; j <= 4; j++)
-        {
-            cout << dp[i][j];
-        }   
-        cout << endl;
-    }
+    // for (int i = 1; i <= 3; i++)
+    // {
+    //     for (int j = 1; j <= 4; j++)
+    //     {
+    //         cout << dp[i][j];
+    //     }   
+    //     cout << endl;
+    // }
 
     answer = dp[n][m];
     return answer;
