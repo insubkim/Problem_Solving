4411d6250b041349fd98d69c049378f8a65ec209
insub <j.j.kant1948@gmail.com>
2026-01-03 14:50:07 +0900

    solved: boj N과M(3)

---
+++ BOJ/N과 M(3).cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int N,M;
+
+int print[9];
+
+void    solve(int round)
+{
+    if (round == M)
+    {
+        for (int i = 0; i < M; i++)
+            cout << print[i] << (i == (M - 1) ? "" : " ");
+        cout << "\n"; 
+        return;
+    }
+
+    for (int i = 1; i <= N; i++)
+    {
+        print[round] = i; // 현재 라운드의 수열 숫자 추가
+        solve(round + 1);
+
+    }
+}
+
+int main()
+{
+    cin >> N >> M;
+ 
+    solve(0);
+}
