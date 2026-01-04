31f2f85fa91f78bda24ae01aac830064f820d6ff
insub <j.j.kant1948@gmail.com>
2026-01-04 14:36:32 +0900

    solved: boj DFS와 BFS

---
+++ BOJ/DFS와 BFS.cpp
@@ -0,0 +1,82 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int N, M, V;
+
+int edge[1001][1001];
+int visited[1001];
+
+void    dfs(int cur_node)
+{
+    // 현제 노드 방문
+    cout << cur_node << " ";
+    visited[cur_node] = 1;
+
+    // 1 ~ N 까지 차례로 방문
+    // dfs 실행
+    
+    for (int i = 1; i <= N; i++)
+    {
+        if (edge[cur_node][i] == 0) continue;
+
+        if (visited[i]) continue;
+
+        dfs(i);
+    }
+}
+
+void    bfs(int cur_node, queue<int> visit_list)
+{
+    // 현재 노드 큐에 추가
+    visit_list.push(cur_node);
+    visited[cur_node] = 1;
+    cout << cur_node << " ";
+
+
+    // 큐 확인
+    // 큐에서 맨앞빼고 전부 방문 처리
+    // 이전에 방문 안했으면 queue 추가
+    while (visit_list.size())
+    {
+        cur_node = visit_list.front();
+        visit_list.pop();
+
+
+        for (int i = 1; i <= N; i++)
+        {
+            if (edge[cur_node][i] == 0) continue;
+
+            if (visited[i]) continue;
+
+            visited[i] = 1;
+            cout << i << " ";
+            
+
+            visit_list.push(i);
+        }
+
+    }   
+}
+
+int main()
+{
+    cin >> N >> M >> V;
+
+    for (int i = 0; i < M; i++)
+    {
+        int a,b;
+        cin >> a >> b;
+        edge[a][b] = 1;
+        edge[b][a] = 1;
+    }
+
+    dfs(V);
+    cout << "\n";
+
+    memset(visited, 0, sizeof(visited));
+    queue<int> visit_list;
+    bfs(V, visit_list);
+    cout << "\n";
+
+}
