60ff0b77596c30e0e90641a87897c63725652017
insub <j.j.kant1948@gmail.com>
2026-01-04 14:47:28 +0900

    refactor: DFS와 BFS

--- BOJ/DFS와 BFS.cpp
+++ BOJ/DFS와 BFS.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 int N, M, V;
 
-int edge[1001][1001];
+vector<int> adj[1001];
 int visited[1001];
 
 void    dfs(int cur_node)
@@ -16,18 +16,16 @@ void    dfs(int cur_node)
     // 1 ~ N 까지 차례로 방문
     // dfs 실행
     
-    for (int i = 1; i <= N; i++)
+    for (auto &x: adj[cur_node])
     {
-        if (edge[cur_node][i] == 0) continue;
-
-        if (visited[i]) continue;
-
-        dfs(i);
+        if (visited[x] == 0)
+            dfs(x);
     }
 }
 
-void    bfs(int cur_node, queue<int> visit_list)
+void    bfs(int cur_node)
 {
+    queue<int> visit_list;
     // 현재 노드 큐에 추가
     visit_list.push(cur_node);
     visited[cur_node] = 1;
@@ -37,23 +35,20 @@ void    bfs(int cur_node, queue<int> visit_list)
     // 큐 확인
     // 큐에서 맨앞빼고 전부 방문 처리
     // 이전에 방문 안했으면 queue 추가
-    while (visit_list.size())
+    while (visit_list.empty() == false)
     {
         cur_node = visit_list.front();
         visit_list.pop();
 
-
-        for (int i = 1; i <= N; i++)
+        for(auto &x : adj[cur_node])
         {
-            if (edge[cur_node][i] == 0) continue;
-
-            if (visited[i]) continue;
-
-            visited[i] = 1;
-            cout << i << " ";
+            if (visited[x])
+                continue;
             
-
-            visit_list.push(i);
+            visited[x] = 1;
+            cout << x << " ";
+            
+            visit_list.push(x);
         }
 
     }   
@@ -67,16 +62,18 @@ int main()
     {
         int a,b;
         cin >> a >> b;
-        edge[a][b] = 1;
-        edge[b][a] = 1;
+        adj[a].push_back(b);
+        adj[b].push_back(a);
     }
+
+    for (int i = 1; i <= N; i++)
+        sort(adj[i].begin(), adj[i].end());
 
     dfs(V);
     cout << "\n";
 
     memset(visited, 0, sizeof(visited));
-    queue<int> visit_list;
-    bfs(V, visit_list);
+    bfs(V);
     cout << "\n";
 
 }
