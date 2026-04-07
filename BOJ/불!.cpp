0637230360577612327b7072af711f53c2b739da
insub <j.j.kant1948@gmail.com>
2026-04-07 18:19:21 +0900

    solved:  BOJ 불

--- BOJ/불!.cpp
+++ BOJ/불!.cpp
@@ -2,118 +2,125 @@
 #include <queue>
 #include <tuple>
 #include <string>
-
 #include <string.h>
 
 using namespace std;
 
 // 행 열 수
-int r,c;
+int r, c;
 
-//#, J, F, .
-//벽, 지훈 위치, 불 위치, 길
+// #, J, F, .
+// 벽, 지훈 위치, 불 위치, 길
 char map[1001][1001];
-
-char fire_snapshot[1001][1001][1001];
 
 bool visited[1001][1001];
 
-void    simulate_fire(void) {
-    // 초기값 복사
-    memcpy(fire_snapshot, map, sizeof(char) * 1001 * 1001);
+// 각 칸에 불이 처음 도착하는 시간
+// -1이면 불이 오지 않음
+int fire_time[1001][1001];
 
-    // 불 확산 시뮬
-    bool fire_flag = true;
-    int time = 0;
-    while (fire_flag) {
-        fire_flag = false;
-        time++;
-        for (int i = 1; i <= r; i++) {
-            for (int j = 1; j <= c; j++) {
-                fire_snapshot[time][i][j] = fire_snapshot[time - 1][i][j];
+void simulate_fire(void) {
+    queue<pair<int, int>> q;
 
-                if (fire_snapshot[time - 1][i][j] != 'F') {
-                    continue ;
-                }
+    memset(fire_time, -1, sizeof(fire_time));
 
-                int dy[4] = {0, -1, 0, 1};
-                int dx[4] = {-1, 0, +1, 0};
-                for (int k = 0; k < 4; k++) {
-                    int y, x;
-                    y = i + dy[k];
-                    x = j + dx[k];
+    // 초기 불 위치 전부 큐에 삽입
+    for (int i = 1; i <= r; i++) {
+        for (int j = 1; j <= c; j++) {
+            if (map[i][j] == 'F') {
+                q.push({i, j});
+                fire_time[i][j] = 0;
+            }
+        }
+    }
 
-                    if (y < 0 || y > r) {
-                        continue;
-                    }
+    // 불 BFS
+    while (!q.empty()) {
+        pair<int, int> cur = q.front();
+        q.pop();
 
-                    if (x < 0 || x > c) {
-                        continue;
-                    }
+        int y = cur.first;
+        int x = cur.second;
 
-                    if (fire_snapshot[time][y][x] != '.') {
-                        continue ;
-                    }
-                    fire_snapshot[time][y][x] = 'F';
-                    fire_flag = true;
-                    }   
-                }
+        int dy[4] = {0, -1, 0, 1};
+        int dx[4] = {-1, 0, 1, 0};
+
+        for (int k = 0; k < 4; k++) {
+            int yy = y + dy[k];
+            int xx = x + dx[k];
+
+            if (yy < 1 || yy > r) {
+                continue;
+            }
+            if (xx < 1 || xx > c) {
+                continue;
+            }
+
+            if (map[yy][xx] == '#') {
+                continue;
+            }
+
+            if (fire_time[yy][xx] != -1) {
+                continue;
+            }
+
+            fire_time[yy][xx] = fire_time[y][x] + 1;
+            q.push({yy, xx});
         }
     }
 }
 
-
 int bfs(int y, int x) {
-    // 시간별 화재 확산 시뮬레이션
+    // 시간별 화재 확산 시뮬 대신
+    // 각 칸에 불 도착 시간을 계산
     simulate_fire();
-    
+
     // 지훈이 탈출 실행
     // 지훈 위치(y,x), 시간
     queue<tuple<int, int, int>> q;
     q.push({y, x, 0});
     visited[y][x] = true;
-    
+
     while (!q.empty()) {
-        tuple<int, int, int> cur;
-        cur = q.front();
+        tuple<int, int, int> cur = q.front();
         q.pop();
 
-        int y = std::get<0>(cur);
-        int x = std::get<1>(cur);
-        int time = std::get<2>(cur);
-    
+        int cy = get<0>(cur);
+        int cx = get<1>(cur);
+        int time = get<2>(cur);
+
         int dy[4] = {0, -1, 0, 1};
-        int dx[4] = {-1, 0, +1, 0};
+        int dx[4] = {-1, 0, 1, 0};
+
         for (int i = 0; i < 4; i++) {
-            int yy, xx;
-            yy = y + dy[i];
-            xx = x + dx[i];
-            
+            int yy = cy + dy[i];
+            int xx = cx + dx[i];
+
+            // 맵 밖으로 나가면 탈출 성공
             if (yy < 1 || yy > r) {
                 return time + 1;
             }
-            
             if (xx < 1 || xx > c) {
                 return time + 1;
             }
-            
+
             if (visited[yy][xx]) {
-                continue ;
+                continue;
             }
 
             if (map[yy][xx] != '.') {
-                continue ;
+                continue;
             }
 
-            if (time <= 1000 && fire_snapshot[time][yy][xx] != '.') {
-                continue ;
+            // 지훈이 다음 칸에 time+1 에 도착
+            // 불이 그보다 먼저 오거나 동시에 오면 못 감
+            if (fire_time[yy][xx] != -1 && fire_time[yy][xx] <= time + 1) {
+                continue;
             }
 
             q.push({yy, xx, time + 1});
             visited[yy][xx] = true;
-
         }
-
     }
 
     return -1;
@@ -123,10 +130,10 @@ int main() {
     cin >> r >> c;
 
     // 지훈 위치
-    int y,x; 
+    int y, x;
     for (int i = 1; i <= r; i++) {
         string line;
-        cin >> line;    
+        cin >> line;
         for (int j = 1; j <= c; j++) {
             map[i][j] = line[j - 1];
             if (map[i][j] == 'J') {
@@ -136,8 +143,7 @@ int main() {
         }
     }
 
-    int min_time;
-    min_time = bfs(y, x);
+    int min_time = bfs(y, x);
 
     if (min_time == -1) {
         cout << "IMPOSSIBLE" << endl;
