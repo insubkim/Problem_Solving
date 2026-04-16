26084678c4f1373d2ed5326c25904d12122b49aa
insub <j.j.kant1948@gmail.com>
2026-04-16 12:52:14 +0900

    solved: BOJ 새싹

---
+++ BOJ/새싹.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <queue>
+
+#include <string.h>
+
+using namespace std;
+
+int main() {
+    char leaf[256] = {
+"         ,r'\"7\n"
+"r`-_   ,'  ,/\n"
+" \\. \". L_r'\n"
+"   `~\\/\n"
+"      |\n"
+"      |"
+    };
+    cout << leaf << endl;
+}
