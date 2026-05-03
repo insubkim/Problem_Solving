ec1650c49ff2b0db1ddaff056d2820db02038df4
insub <j.j.kant1948@gmail.com>
2026-05-03 10:50:10 +0900

    solved: codeforce Watermelon

---
+++ Code Force/A/Watermelon.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"bufio"
+)
+
+func main() {
+	r := bufio.NewReader(os.Stdin)
+	w := bufio.NewWriter(os.Stdout)
+	defer w.Flush()
+
+	var weigh int
+	fmt.Fscan(r, &weigh)
+
+	if weigh % 2 != 0 || weigh == 2 {
+		fmt.Fprintf(w, "NO\n")
+	} else {
+		fmt.Fprintf(w, "YES\n")
+	}
+
+}
