0b2ddba68d0210d5feae5960dff9fbf659e2427b
root <j.j.kant1948@gmail.com>
2026-05-22 12:08:22 +0900

    solved: codeforce A. Contruct an Array

---
+++ Code Force/Contest/round 1099/A. Construct an Array.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+	int t;
+	cin >> t;
+
+	while (t--) {
+
+		int n;
+		cin >> n;
+		for (int i = 1; i <= n; i++) {
+			cout << i*2 - 1<< ' ';
+		}
+		cout << '\n';
+	}
+
+}
